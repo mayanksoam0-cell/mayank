4bcff7aec68b5014e5078bed4a140c13b3d943ce
mayanksoam0-cell <mayanksoam0@gmail.com>
2025-08-16 23:53:49 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        richGreen: "#0a3d2f",
+        cream: "#f9f7f3",
+        gold: "#c5a46d"
+      },
+      fontFamily: {
+        serif: ["Cormorant Garamond", "serif"],
+        sans: ["Work Sans", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+}
